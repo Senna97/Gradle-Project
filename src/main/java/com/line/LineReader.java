49eac7c820e8e6669eec4fc9f2935ff1bab8954a
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-13 13:11:21 +0900

    Commit

--- src/main/java/com/line/LineReader.java
+++ src/main/java/com/line/LineReader.java
@@ -5,9 +5,6 @@ import com.line.parser.Parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -3,7 +3,11 @@ package com.line.parser;
 import com.line.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital> {
-    
+
+    private String replaceAllQuot(String str) {
+        return str.replaceAll("\"", "");
+    }
+
     @Override
     public Hospital parse(String str) {
         String[] splitted = str.split(",");
