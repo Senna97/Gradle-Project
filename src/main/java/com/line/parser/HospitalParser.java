765bff5d10640880a474bcfae373d04611ab2262
Seohyeon <wkdtjgus0319@skuniv.ac.kr>
2022-10-13 13:36:30 +0900

    Commit

--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -6,7 +6,6 @@ public class HospitalParser implements Parser<Hospital> {
 
     @Override
     public Hospital parse(String str) {
-        str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
         return new Hospital(splitted[0], splitted[1], splitted[1], splitted[2], splitted[6], splitted[10], splitted[0]);
     }
