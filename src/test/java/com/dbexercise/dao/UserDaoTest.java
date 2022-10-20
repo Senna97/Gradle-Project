c4bf7c2217c8c7c14b21652783ec44d783cd58aa
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-10-20 14:03:14 +0900

    Test Refactoring

--- src/test/java/com/dbexercise/dao/UserDaoTest.java
+++ src/test/java/com/dbexercise/dao/UserDaoTest.java
@@ -1,7 +1,9 @@
 package com.dbexercise.dao;
 
 import com.dbexercise.domain.User;
-import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,22 +23,36 @@ class UserDaoTest {
     @Test
     public void addAndGet() throws SQLException, ClassNotFoundException {
         UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        User user = new User();
 
-        user.setId("0");
-        user.setName("seohyeon");
-        user.setPassword("0000");
+        User user = new User("0", "seohyeon", "0000");
         userDao.add(user);
         userDao.deleteAll();
-        Assertions.assertEquals(0, userDao.getCount());
+        assertEquals(0, userDao.getCount());
 
-        user.setId("1");
-        user.setName("seohyeon");
-        user.setPassword("0000");
-        userDao.add(user);
-        Assertions.assertEquals(1, userDao.getCount());
+        User user1 = new User("1", "seohyeon", "0000");
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
 
         User result = userDao.get("1");
-        Assertions.assertEquals("1", result.getId());
+        assertEquals("1", result.getId());
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        User user2 = new User("2", "seohyeon", "0000");
+        User user3 = new User("3", "seohyeon", "0000");
+        User user4 = new User("4", "seohyeon", "0000");
+
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user2);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user4);
+        assertEquals(3, userDao.getCount());
     }
 }
