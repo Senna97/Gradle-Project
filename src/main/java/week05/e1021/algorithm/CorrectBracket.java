15b905f445693f3cc36d17ca04e9600364e62b89
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-10-21 10:48:11 +0900

    CorrectBracket Commit

--- src/main/java/week05/e1021/algorithm/CorrectBracket.java
+++ src/main/java/week05/e1021/algorithm/CorrectBracket.java
@@ -5,19 +5,18 @@ import java.util.Stack;
 public class CorrectBracket {
 
     boolean solution(String s) {
-
         Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
-            if ('(' == s.charAt(i)) {
+            if (s.charAt(i) == '(') {
                 stack.push(s.charAt(i));
-            } else if (')' == s.charAt(i)) {
+            } else if (s.charAt(i) == ')') {
                 if (stack.empty()) {
                     return false;
                 }
                 stack.pop();
             }
         }
-
         return stack.empty();
     }
 }
